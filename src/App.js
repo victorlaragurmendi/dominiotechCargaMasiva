615356d3b3d6565921973276ba0f517799e3e14e
victorlara123 <victor.lara@unmsm.edu.pe>
2021-12-06 13:16:43 -0500

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+
+  const SeleccionarArchivo = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+  };
+
+
+  const CargarArchivo = function () {
+    console.log("se cargo el archivo");
+  }
+ 
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedorExportMasiva">
+        <span> <p>CARGA MASIVA :OBJETOS MODIFICADOS</p></span>
+
+        <span><input type="file" name="file" onChange={SeleccionarArchivo} />
+            {isFilePicked ? (
+              <div>
+                <p>Filename: {selectedFile.name}</p>
+                <p>Filetype: {selectedFile.type}</p>
+                <p>Size in bytes: {selectedFile.size}</p>
+                <p>
+                  lastModifiedDate:{' '}
+                  {selectedFile.lastModifiedDate.toLocaleDateString()}
+                </p>
+              </div>
+            ) : (
+              <p>Select a file to show details</p>
+            )}
+        </span>
+        <span><button onClick={CargarArchivo} >Cargar Archivo</button></span>
+        <span><p>PLantilla de ejemplo  Carga Masiva Temporal <a>Descargar</a></p></span>
+      </div>
     </div>
   );
 }
