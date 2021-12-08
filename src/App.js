752a36c6a67db9300f473487e327b82cb3de57cc
victorlara123 <victor.lara@unmsm.edu.pe>
2021-12-08 09:49:52 -0500

    drag n drop

--- src/App.js
+++ src/App.js
@@ -2,46 +2,116 @@
 import './App.css';
 import React, { useState } from 'react';
 
+// librerias para arrastrar archivos 
+import {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+
+//libreria para  barra de carga 
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+
+
+
+
+
 function App() {
 
-
-  const [selectedFile, setSelectedFile] = useState();
-  const [isFilePicked, setIsFilePicked] = useState(false);
-
-  const SeleccionarArchivo = (event) => {
-    setSelectedFile(event.target.files[0]);
-    setIsFilePicked(true);
-  };
+  //funcion para gestionar archivos aceptados
+  const onDrop = useCallback(acceptedFiles => {
+    console.log(acceptedFiles);
+  }, [])
 
 
-  const CargarArchivo = function () {
-    console.log("se cargo el archivo");
+
+
+  // logica  arrastrar archivos
+  const maxLength = 20;
+
+  function nameLengthValidator(file) {
+    if (file.name.length > maxLength) {
+      return {
+        code: "name-too-large",
+        message: `Name is larger than ${maxLength} characters`
+      };
+    }
+  
+    return null
   }
+
+
+  const {
+    acceptedFiles,
+    fileRejections,
+    open,
+    getRootProps,
+    getInputProps
+  } = useDropzone({
+    validator: nameLengthValidator
+  });
+
+  const acceptedFileItems = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  const fileRejectionItems = fileRejections.map(({ file, errors }) => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+      <ul>
+        {errors.map(e => (
+          <li key={e.code}>{e.message}</li>
+        ))}
+      </ul>
+    </li>
+  ));
+  
  
   
+  //  barra de carga
+  const percentage = 59;
+  const needDominantBaselineFix = true;
+
+
 
   return (
     <div className="App">
       <div className="contenedorExportMasiva">
         <span> <p>CARGA MASIVA :OBJETOS MODIFICADOS</p></span>
 
-        <span><input type="file" name="file" onChange={SeleccionarArchivo} />
-            {isFilePicked ? (
-              <div>
-                <p>Filename: {selectedFile.name}</p>
-                <p>Filetype: {selectedFile.type}</p>
-                <p>Size in bytes: {selectedFile.size}</p>
-                <p>
-                  lastModifiedDate:{' '}
-                  {selectedFile.lastModifiedDate.toLocaleDateString()}
-                </p>
-              </div>
-            ) : (
-              <p>Select a file to show details</p>
-            )}
-        </span>
-        <span><button onClick={CargarArchivo} >Cargar Archivo</button></span>
+       
+    <div className="contenedorDragDrop">
+        <div   {...getRootProps({ className: 'dropzone' })}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop some files here, or click to select files</p>
+          <em>(Only files with name less than 20 characters will be accepted)</em>
+        </div>
+        <button type="button" onClick={open}>
+            Open File Dialog
+        </button>
+    </div>   
+      <aside>
+        <h4>Accepted files</h4>
+        <ul>{acceptedFileItems}</ul>
+        <h4>Rejected files</h4>
+        <ul>{fileRejectionItems}</ul>
+      </aside>
+    
+
+        <span><button onClick={onDrop} >Cargar Archivo</button></span>
         <span><p>PLantilla de ejemplo  Carga Masiva Temporal <a>Descargar</a></p></span>
+
+     
+       <div className="BarraCarga">
+          <CircularProgressbar
+            value={percentage}
+            text={
+              <tspan dy={needDominantBaselineFix ? -15 : 0}>{percentage}%</tspan>
+            }
+          />
+       </div>
+
       </div>
     </div>
   );
